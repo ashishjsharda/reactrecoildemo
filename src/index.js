6ce889681d2512ec1223e4a9f02c978c131ab944
Ashish Sharda <ashishjsharda@gmail.com>
2023-02-18 06:16:48 -0600

    add textinput

--- src/index.js
+++ src/index.js
@@ -20,3 +20,19 @@ function CharacterCounter() {
         </div>
     );
 }
+
+function TextInput() {
+    const [text, setText] = useRecoilState(textState);
+
+    const onChange = (event) => {
+        setText(event.target.value);
+    };
+
+    return (
+        <div>
+            <input type="text" value={text} onChange={onChange} />
+            <br />
+            Echo: {text}
+        </div>
+    );
+}
